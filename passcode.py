68c5090df6528d21e8302e402f307246d4b95356
Pranith Kumar <148210671+pranithaiwiz@users.noreply.github.com>
2023-12-04 12:56:00 +0530

    Update passcode.py

--- passcode.py
+++ passcode.py
@@ -1,5 +1,5 @@
 import random
-chars = "1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p;/-['=]!QAZ@WSX#EDC$RFV%TGB^YHN&UJM*IK<(OL>)P:?_{+}|\"
+chars = "1qaz2wsx3edc4rfv5tgb6yhn7ujm8ik,9ol.0p;/-['=]!QAZ@WSX#EDC$RFV%TGB^YHN&UJM*IK<(OL>)P:?_{+}|\""
 length = int(input("Enter the length of the password: "))
 password = ""
 for a in range(length):
